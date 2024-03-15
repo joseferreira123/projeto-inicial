dc6f0fb8cd0e1de4ff3d68e432d2acbdbd20ef43
joseferreira123 <mcferdequeiroz@gmail.com>
2024-03-15 08:24:13 -0300

    variavel nome

--- java.js
+++ java.js
@@ -1,6 +1,12 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("voce clicou no botao" + event);
+    alert("voce clicou no botao" + nome + " " + nome.value);
+    var numero = 7;
+    var resultado = numero % 2;
+    if(resultado == 0){
+        alert("este numero é par");
+    }
 }
